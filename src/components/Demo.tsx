ed157d22bf619329f9e24ef6a015d194f12c9d37
Ashish911 <ashishdongol32@gmail.com>
2023-09-09 16:08:56 +0545

    Completed but learnt only little. Can be further helpfull for summarizing.

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,15 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { linkIcon } from "../assets";
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo: React.FC = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(article);
+  }, [article]);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const existingArticle = allArticles.find(
+      (item) => item.url === article.url
+    );
+
+    if (existingArticle) return setArticle(existingArticle);
+
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      // update state and local storage
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
+  };
+
+  const handleCopy = async (e: any) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  const handleKeyDown = async (e: any) => {
+    if (e.keyCode === 13) {
+      handleSubmit(e);
+    }
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
-      {/* Search */}
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          // onSubmit={handleSubmit}
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
@@ -20,9 +78,9 @@ const Demo: React.FC = () => {
           <input
             type="url"
             placeholder="Paste the article link"
-            // value={article.url}
-            // onChange={(e) => setArticle({ ...article, url: e.target.value })}
-            // onKeyDown={handleKeyDown}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            onKeyDown={handleKeyDown}
             required
             className="url_input peer" // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
           />
@@ -34,9 +92,8 @@ const Demo: React.FC = () => {
           </button>
         </form>
 
-        {/* Browse History */}
         <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
-          {/* {allArticles.reverse().map((item, index) => (
+          {allArticles.reverse().map((item, index) => (
             <div
               key={`link-${index}`}
               onClick={() => setArticle(item)}
@@ -53,13 +110,13 @@ const Demo: React.FC = () => {
                 {item.url}
               </p>
             </div>
-          ))} */}
+          ))}
         </div>
       </div>
 
       {/* Display Result */}
       <div className="my-10 max-w-full flex justify-center items-center">
-        {/* {isFetching ? (
+        {isFetching ? (
           <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
         ) : error ? (
           <p className="font-inter font-bold text-black text-center">
@@ -82,7 +139,7 @@ const Demo: React.FC = () => {
               </div>
             </div>
           )
-        )} */}
+        )}
       </div>
     </section>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { logo } from "../assets";
 
-const Hero = () => {
+const Hero: React.FC = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
